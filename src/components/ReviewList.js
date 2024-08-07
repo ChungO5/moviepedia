678a179b8adcd38208004daf59ff5cb81772c2f2
KimChungO <kco796@naver.com>
2024-08-07 18:44:43 +0900

    별점 컴포넌트 만들기

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,39 +1,44 @@
 import React from "react";
 import "./ReviewList.css";
+import Rating from "./Rating";
 
 const formatDate = (value) => {
-  const date = new Date(value);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+    const date = new Date(value);
+    return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 };
 
 const ReviewListItem = ({ item, onDelete }) => {
-  const handleDeleteClick = () => onDelete(item.id);
-  return (
-    <div className="ReviewListItem">
-      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
-      <div>
-        <h1>{item.title}</h1>
-        <p>{item.rating}</p>
-        <p>{formatDate(item.createdAt)}</p>
-        <p>{item.content}</p>
-        <button onClick={handleDeleteClick}>삭제</button>
-      </div>
-    </div>
-  );
+    const handleDeleteClick = () => onDelete(item.id);
+    return (
+        <div className="ReviewListItem">
+            <img
+                className="ReviewListItem-img"
+                src={item.imgUrl}
+                alt={item.title}
+            />
+            <div>
+                <h1>{item.title}</h1>
+                <Rating value={item.rating} />
+                <p>{formatDate(item.createdAt)}</p>
+                <p>{item.content}</p>
+                <button onClick={handleDeleteClick}>삭제</button>
+            </div>
+        </div>
+    );
 };
 
 const ReviewList = ({ items, onDelete }) => {
-  return (
-    <ul>
-      {items.map((item) => {
-        return (
-          <li key={item.id}>
-            <ReviewListItem item={item} onDelete={onDelete} />
-          </li>
-        );
-      })}
-    </ul>
-  );
+    return (
+        <ul>
+            {items.map((item) => {
+                return (
+                    <li key={item.id}>
+                        <ReviewListItem item={item} onDelete={onDelete} />
+                    </li>
+                );
+            })}
+        </ul>
+    );
 };
 
 export default ReviewList;
