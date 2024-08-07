1b738c245a993220b83e2e18df6096244db7a676
KimChungO <kco796@naver.com>
2024-08-07 18:19:55 +0900

    ref로 DOM 노드 가져오기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const FileInput = ({ name, value, onChange }) => {
+    const inputRef = useRef();
+
     const handleChange = (e) => {
         const nextValue = e.target.files[0];
         onChange(name, nextValue);
     };
-    return <input type="file" onChange={handleChange} />;
+
+    useEffect(() => {
+        if (inputRef.current) {
+            console.log(inputRef);
+        }
+    }, []);
+
+    return <input type="file" onChange={handleChange} ref={inputRef} />;
 };
 
 export default FileInput;
